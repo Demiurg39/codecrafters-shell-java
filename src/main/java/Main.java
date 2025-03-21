9af0705ae86fa5f4c8f549700db3fbf33934a5ba
Demiurg39 <chiryagov2014@gmail.com>
2025-03-21 14:40:06 +0300

    exit func added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
+    public static void exit(String[] args) {
+        scanner.close();
+        System.exit((args.length > 1) ? Integer.parseInt(args[1]) : 0);
+    }
+
+    public static void main(String[] args) throws Exception {
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
             String[] tokens = input.split("\\s");
 
-            if (tokens[0].equals("exit"))
-                break;
-
-            System.out.println(input + ": command not found");
-
+            switch(tokens[0]) {
+                case "exit": exit(tokens); break;
+                default: System.out.println(input + ": command not found");
+            };
         }
-
     }
 }
